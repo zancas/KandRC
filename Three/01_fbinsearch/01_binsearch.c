ae37901aad42ab2fba4e31266fbe6bc43c709a4a
zancas <zancas@leastauthority.com>
2012-01-20 18:20:31 -0800

    Added time.h-->clock based timing.  Notice fbinsearch isn't always faster.. e.g. for the value picked.

--- Three/01_fbinsearch/01_binsearch.c
+++ Three/01_fbinsearch/01_binsearch.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 /* fbinsearch: find x in v[0] <= v[1] ... <= v[n-1] */
 
@@ -9,22 +10,41 @@ int binsearch(int x, int v[], int n);
 main() 
 {
   int result, targval, i, n;
-  targval = 12;
-  n = 5;
+  int binsearchtime, fbinsearchtime, baseline;
+  double sbinsearchtime, sfbinsearchtime;
+  targval = 72;
+  n = 50;
   int numarray[n];
   for (i=0; i < n; i++) {
     numarray[i] = i*3;
-    printf("numarray[%d] = %d\n",i, numarray[i]);
+    printf("For binsearch: numarray[%d] = %d\n",i, numarray[i]);
   }
 
   int counter = 0;
+  baseline = clock();
   while (counter < 100000000) {
     result = binsearch(targval, numarray, n);
     counter++;
   }
+  binsearchtime = clock() - baseline;
+  sbinsearchtime = binsearchtime / (double)CLOCKS_PER_SEC;
+  printf("Total time for 100000000 binsearches = %3.3f\n", sbinsearchtime);
   printf("The value is at index = %d; Where targval is %d\n",result, targval);
+  
+
+  counter = 0;
+  baseline = clock();
+  while (counter < 100000000) {
+    result = fbinsearch(targval, numarray, n);
+    counter++;
+  }
+  fbinsearchtime = clock() - baseline;
+  sfbinsearchtime = fbinsearchtime / (double)CLOCKS_PER_SEC;
+  printf("Total time for 100000000 fbinsearches = %3.3f\n", sfbinsearchtime);
+  printf("For fbinsearch: The value is at index = %d; Where targval is %d\n",result, targval);
 
     
+  printf("The result of calling clock/CLOCKS_PER_SEC: %3.3f\n", clock()/(double)CLOCKS_PER_SEC);
   return 0;
 }
 
