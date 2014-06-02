d931145cdf4d07f7d5da7189cc19f0bacf036789
zancas <zancas@leastauthority.com>
2014-06-01 18:14:20 -0600

    indeed getchar() != EOF, is either 0 or 1

--- One/06_verifyEOFval/verifyEOFval.c
+++ One/06_verifyEOFval/verifyEOFval.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-/* verify that EOF is 1 or 0 */
- 
+/* verify that getchar() != EOF is 1 or 0 */
+
 main()
 {
   int c;
   int boolean;
-  while ( boolean = (getchar() != EOF) )
+  while ( boolean = ((c = getchar()) != EOF) )
     {
       printf("Value of boolean: %1.0d\n", boolean);
+      printf("c: %c\n", c);
+      printf("c: %d\n", c);
+      printf("----\n");
     }
   printf("Value of boolean after the loop has exited: %d\n", boolean);
+  printf("Numeric value of c after the loop has exited: %d\n", c);
+  printf("Character repr of c after the loop has exited: %c\n", c);
+  printf("Character repr of c after the loop has exited: %x\n", c);
   return 0;
 }
