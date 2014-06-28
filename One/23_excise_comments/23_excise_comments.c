5b03b4c79e7bdebe2f9f5d4c6f89a385c2f570c9
zancas <zancas@leastauthority.com>
2014-06-27 18:27:50 -0600

    Now handles '//'.

--- One/23_excise_comments/23_excise_comments.c
+++ One/23_excise_comments/23_excise_comments.c
@@ -10,10 +10,11 @@ int get_buffer(char s[], int lim);
 char incomingbuffer[TOTALWIDTH];     /* An array that holds the buffer under process. */
 int buffer_length;      /* Where are we in the buffer? */
 
-
+//FOOTBALLFOOLBOOF
 /* Replace spaces with with the appropriate number of tabs. */
 main()
 {
+  int slash_slash = 0;
   int index_inbuffer = 0;
   int index_outbuffer = 0;
   int incomment_index = 0;       /* Counters */
@@ -22,13 +23,16 @@ main()
   int i;
   for(i=0;i<buffer_length;i++) {
     outgoingbuffer[i]='\0';
-    printf("int i is: %i\n", i);
-  }
-  printf("finished array initialization.\n");
-  printf("buffer_length is: %i\n", buffer_length);
+  }//ANOTHER COMMENT
   for (index_inbuffer=0;index_inbuffer < buffer_length; ++index_inbuffer) {
-    printf("index_inbuffer is: %i\n", index_inbuffer);
-    if (INCOMMENT && index_inbuffer > 0) {
+    if (INCOMMENT) {
+      if(slash_slash) {
+	if(incomingbuffer[index_inbuffer] == '\n') {
+	  INCOMMENT = 0;
+	  slash_slash = 0;
+	  outgoingbuffer[index_outbuffer++] = '\n';
+	}/*Also this/* // */
+      }
       if(incomingbuffer[index_inbuffer] == '/' && incomingbuffer[index_inbuffer-1] == '*') {INCOMMENT = 0;}
     } else if (index_inbuffer > 0 && incomingbuffer[index_inbuffer] == '*' && incomingbuffer[index_inbuffer-1] == '/') {
       incomment_index = index_inbuffer;
@@ -36,28 +40,23 @@ main()
 	incomment_index++;
 	if (incomingbuffer[incomment_index] == '/' && incomingbuffer[incomment_index-1] == '*') {
 	  INCOMMENT = 1;
-	  if(index_outbuffer > 0) index_outbuffer--;
+	  index_outbuffer--;
 	  break;
 	}
       }
       if(!INCOMMENT) {
-      printf("index_outbuffer: %i\n", index_outbuffer);
-      printf("index_inbuffer: %i\n", index_inbuffer);
-      outgoingbuffer[index_outbuffer++] = incomingbuffer[index_inbuffer];
-      printf("index_outbuffer: %i\n", index_outbuffer);
+	outgoingbuffer[index_outbuffer++] = incomingbuffer[index_inbuffer];
       }
+    } else if (index_inbuffer > 0 && incomingbuffer[index_inbuffer] == '/' && incomingbuffer[index_inbuffer-1] == '/') {
+      slash_slash = 1;
+      INCOMMENT = 1;
+      index_outbuffer--;
     } else {
-      printf("index_outbuffer: %i\n", index_outbuffer);
-      printf("index_inbuffer: %i\n", index_inbuffer);
       outgoingbuffer[index_outbuffer++] = incomingbuffer[index_inbuffer];
-      printf("index_outbuffer: %i\n", index_outbuffer);
     }
   }
-  printf("buffer_length: %i\n", buffer_length);
-
-  printf("%s\n", outgoingbuffer);
   outgoingbuffer[index_outbuffer] = '\0';
-  printf("%s\n", outgoingbuffer);
+  printf("%s", outgoingbuffer);
   return 0;
 }
 
@@ -68,12 +67,9 @@ int get_buffer(char s[], int lim)
 {
   int c, i;
   for (i=0; i<lim-1 && (c=getchar())!= EOF; i++){
-    printf("%i\n", i);
     s[i] = c;
   }
   s[i++] = '\0';
-  printf("incoming buffer is: %s\n", s);
-  printf("incoming buffer length is: %i\n", i);
   return i;
 }
 
