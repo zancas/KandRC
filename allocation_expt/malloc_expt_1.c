d8e24c892b47a5e929381f42236b247d65e77d5a
zancas <zancas@leastauthority.com>
2014-07-05 17:34:06 -0600

    first draft pack_char_array implemented

---
+++ allocation_expt/malloc_expt_1.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char* pack_char_array(char* char_array_ptr){
+  //Warning assuming '\0' array terminator
+  int char_array_index = 0;
+  while(char_array_ptr[char_array_index] != '\0'){char_array_index++;}
+  char_array_ptr = realloc(char_array_ptr, char_array_index+1);
+  return char_array_ptr;
+}
+
+int main(int argc, char **argv)
+{
+  if(argc != 2){
+    printf("Must pass 1 argument!\n");
+    return -1;
+  }
+  char* packed_array;
+  return 0;
+}
