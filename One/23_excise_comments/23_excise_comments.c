eac079d938a92a467fd06b69caa68076776285f8
zancas <zancas@leastauthority.com>
2014-07-05 23:06:24 -0600

    first draft functional dynamic 23_excise_comments.c

--- One/23_excise_comments/23_excise_comments.c
+++ One/23_excise_comments/23_excise_comments.c
@@ -1,77 +1,108 @@
 #include <stdio.h>
-#define TOTALWIDTH 100000  /* Max size of the input buffer. */
-int INCOMMENT = 0;
+#include <stdlib.h>
+#include <string.h>
 
-// MAKE THIS A FUNCTION TO USE IN 24.
-
-/* The fundamental procedure is that comment beginning '/*''s are used to
+/* '/*''s and '//''s are used to
    indicate comment entry. Comment state is recorded with an INCOMMENT
    boolean flag. putchar is not called while INCOMMENT is 1 */
 
-int get_buffer(char s[], int lim);
-char incomingbuffer[TOTALWIDTH];     /* An array that holds the buffer under process. */
-int buffer_length;      /* Where are we in the buffer? */
+char* read_file_chars_into_heap_array(char* filename, char* heap_chars);
+char* excise_comments(char* code_array, char* excised_array);
 
-//FOOTBALLFOOLBOOF
-/* Replace spaces with with the appropriate number of tabs. */
-main()
+int main(int argc, char** argv)
 {
+  if(argc != 2){
+    printf("You must pass the name of the file to analyze, as the only argument. \n");
+    return -1;
+  }
+
+  printf("Filename: %s\n", argv[1]);
+  char* raw_c_array = (char *) malloc(1);
+  char* commentless_c = (char *) malloc(1);
+  raw_c_array = read_file_chars_into_heap_array(argv[1], raw_c_array);
+  commentless_c = excise_comments(raw_c_array, commentless_c);
+  printf("%s\n", commentless_c);
+  free(commentless_c);
+  free(raw_c_array);
+  return 0;
+}
+
+char* excise_comments(char* code_array, char* excised_array){
+  int buffer_length = 0;
+  int INCOMMENT = 0;
   int slash_slash = 0;
   int index_inbuffer = 0;
   int index_outbuffer = 0;
   int incomment_index = 0;       /* Counters */
-  buffer_length = get_buffer(incomingbuffer, TOTALWIDTH);
-  char outgoingbuffer[buffer_length];
+  buffer_length = strlen(code_array);
+  excised_array = realloc(excised_array, buffer_length);
   int i;
   for(i=0;i<buffer_length;i++) {
-    outgoingbuffer[i]='\0';
+    excised_array[i]='\0';
   }//ANOTHER COMMENT
+
+  //This loop contains the excision logic, the outermost 'if' handles the 4 possible cases
+  // Case (0)  The current index is inside a comment (of some type).
+  // Case (1)  The current index _is_ at the beginning of '//' style comment.
+  // Case (2)  The current index might be at the beginning of a '/**/' style comment.
+  //       (2a) The current index was not the beginning of a '/**/' style comment (no closing '*/').
+  //       (2b) The current index was at the beginning of '/**/' (closing '*/').
+  // Case (3)  The current index is not in a comment.
   for (index_inbuffer=0;index_inbuffer < buffer_length; ++index_inbuffer) {
     if (INCOMMENT) {
       if(slash_slash) {
-	if(incomingbuffer[index_inbuffer] == '\n') {
+	if(code_array[index_inbuffer] == '\n') {
 	  INCOMMENT = 0;
 	  slash_slash = 0;
-	  outgoingbuffer[index_outbuffer++] = '\n';
+	  excised_array[index_outbuffer++] = '\n';
 	}/*Also this/* // */
       }
-      if(incomingbuffer[index_inbuffer] == '/' && incomingbuffer[index_inbuffer-1] == '*') {INCOMMENT = 0;}
-    } else if (index_inbuffer > 0 && incomingbuffer[index_inbuffer] == '*' && incomingbuffer[index_inbuffer-1] == '/') {
+      if(code_array[index_inbuffer] == '/' && code_array[index_inbuffer-1] == '*') {INCOMMENT = 0;}
+    } else if (index_inbuffer > 0 && code_array[index_inbuffer] == '/' && code_array[index_inbuffer-1] == '/') {
+      slash_slash = 1;
+      INCOMMENT = 1;
+      index_outbuffer--;
+    } else if (index_inbuffer > 0 && code_array[index_inbuffer] == '*' && code_array[index_inbuffer-1] == '/') {
       incomment_index = index_inbuffer;
       while (incomment_index < buffer_length) {
 	incomment_index++;
-	if (incomingbuffer[incomment_index] == '/' && incomingbuffer[incomment_index-1] == '*') {
+	if (code_array[incomment_index] == '/' && code_array[incomment_index-1] == '*') {
 	  INCOMMENT = 1;
 	  index_outbuffer--;
 	  break;
 	}
       }
       if(!INCOMMENT) {
-	outgoingbuffer[index_outbuffer++] = incomingbuffer[index_inbuffer];
+	excised_array[index_outbuffer++] = code_array[index_inbuffer];
       }
-    } else if (index_inbuffer > 0 && incomingbuffer[index_inbuffer] == '/' && incomingbuffer[index_inbuffer-1] == '/') {
-      slash_slash = 1;
-      INCOMMENT = 1;
-      index_outbuffer--;
     } else {
-      outgoingbuffer[index_outbuffer++] = incomingbuffer[index_inbuffer];
+      excised_array[index_outbuffer++] = code_array[index_inbuffer];
     }
   }
-  outgoingbuffer[index_outbuffer] = '\0';
-  printf("%s", outgoingbuffer);
-  return 0;
+
+  excised_array[index_outbuffer] = '\0';
+  excised_array = realloc(excised_array, index_outbuffer+1);
+  printf("%s", excised_array);
+  return excised_array;
 }
 
-
-
-/* get_buffer: read a buffer into s, return length */
-int get_buffer(char s[], int lim)
-{
-  int c, i;
-  for (i=0; i<lim-1 && (c=getchar())!= EOF; i++){
-    s[i] = c;
+char* read_file_chars_into_heap_array(char* filename, char* heap_chars){
+  //(filename string, heap_chars pointer) --> ptr_to_heap_chars with file bytes
+  // INVARIANT:  The file is terminated by EOF, and contains chars.
+  FILE* fp;
+  fp = fopen(filename, "r");
+  int i = 0;
+  char c;
+  while(EOF != (c = getc(fp))){i++;}
+  printf("i is: %i\n", i);
+  heap_chars = realloc(heap_chars, i+1);
+  rewind(fp);
+  int k;
+  for(k = 0; k<i; k++){
+    heap_chars[k] = getc(fp);
   }
-  s[i++] = '\0';
-  return i;
+  heap_chars[k] = '\0';
+  fclose(fp);
+  printf("%s\n", heap_chars);
+  return heap_chars;
 }
-
