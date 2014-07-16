5831d86a0e7230e4087c4866991fa10a700b2481
zancas <zancas@leastauthority.com>
2014-07-15 22:18:38 -0600

    minor style improvement

--- Two/10_lowerwithconditional/lower.c
+++ Two/10_lowerwithconditional/lower.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-int lower(int x);
-main() 
+char lower(char x);
+int main(int argc, char** argv)
 {
-  
-  putchar(lower('x'));
+  if( argc != 2 ) {
+    printf("Usage: arg 1 = char to be lowercased\n");
+    return -1;
+  }
+  printf("Result: ");
+  putchar(lower(argv[1][0]));
+  printf("\n");
   return 0;
 }
 
-int lower(int x) {
-  int temp;
-  temp = (x >= 'A' && x <= 'Z') ? x+'a'-'A' :  x ;
-  return temp;
+char lower(char x) {
+  return (x >= 'A' && x <= 'Z') ? x+'a'-'A' :  x ;
 }
