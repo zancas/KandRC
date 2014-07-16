b15727e128517969830dd88819b754d85647533b
zancas <zancas@leastauthority.com>
2014-07-15 22:10:25 -0600

    use cli params, minor cleanup

--- Two/09_fastbitcount/fastbitcount.c
+++ Two/09_fastbitcount/fastbitcount.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int fbitcount(unsigned i);
 int bitcount(unsigned x);
 int main(int argc, char** argv)
 {
-  int BIGCOUNTER = 21474830;
-  if (argv[1] == NULL){
-    printf("NO!\n");
-    return 15;
+  if( argc != 3 ) {
+    printf("Usage: arg 1 = {classic,fast}, arg 2 = n: integer whose bits will be counted\n");
+    return -1;
   }
+  int BIGCOUNTER = 21474830;
   if (strcmp(argv[1], "fast") == 0) {
     while (BIGCOUNTER > 0) {
-      fbitcount(2147483647);
+      fbitcount(atoi(argv[2]));
       BIGCOUNTER--;
     }
   }else if(strcmp(argv[1], "classic") == 0) {
     while (BIGCOUNTER > 0) {
-      bitcount(2147483647);
+      bitcount(atoi(argv[2]));
       BIGCOUNTER--;
     }
   }
-  printf("bitcount of %d is %d.\n",2147483647, bitcount(2147483647));
-  printf("fbitcount of %d is %d.\n",2147483647, fbitcount(2147483647));
-  /*printf("bitcount of %d is %d.\n",2147483647, bitcount(2147483647));
-  printf("bitcount of %d is %d.\n",8, bitcount(8));
-  printf("fbitcount of %d is %d.\n",8, fbitcount(8));
-  printf("bitcount of %d is %d.\n",1, bitcount(1));
-  printf("fbitcount of %d is2 %d.\n",1, fbitcount(1));
-  printf("bitcount of %d is %d.\n",16, bitcount(16));
-  printf("fbitcount of %d is %d.\n",16, fbitcount(16));
-  printf("bitcount of %d is %d.\n",15, bitcount(15));
-  printf("fbitcount of %d is %d.\n",15, fbitcount(15));
-  printf("bitcount of %d is %d.\n",32, bitcount(32));
-  printf("fbitcount of %d is %d.\n",32, fbitcount(32));
-  printf("bitcount of %d is %d.\n",3, bitcount(3));
-  printf("fbitcount of %d is %d.\n",3, fbitcount(3));
-  */
   return 0;
 }
 
